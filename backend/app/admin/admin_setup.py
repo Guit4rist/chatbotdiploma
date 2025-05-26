6f468403cf1cfd9c733c40a1c7ecec1f6371d32a
rauan <salkenov.rauan7@gmail.com>
2025-05-26 14:02:55 +0500

    User badge change

--- backend/app/admin/admin_setup.py
+++ backend/app/admin/admin_setup.py
@@ -1,4 +1,3 @@
-from app.models.badge import Badge
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
 from app.db.dependencies import get_db
@@ -12,20 +11,20 @@ router.include_router(logs.router)
 def admin_home():
     return {"message": "Welcome to the admin panel! (Basic placeholder)"}
 
-@router.post("/setup-default-badges/")
-def setup_default_badges(db: Session = Depends(get_db)):
-    default_badges = [
-        {"name": "First 100 XP", "description": "You reached 100 XP!", "icon": "🏅"},
-        {"name": "Chatterbox", "description": "You’ve sent 50 messages!", "icon": "💬"},
-        {"name": "Level Up!", "description": "You reached your first new level!", "icon": "📈"},
-    ]
-
-    created = 0
-    for badge_data in default_badges:
-        exists = db.query(Badge).filter(Badge.name == badge_data["name"]).first()
-        if not exists:
-            db.add(Badge(**badge_data))
-            created += 1
-
-    db.commit()
-    return {"message": f"{created} badges added successfully."}
+#@router.post("/setup-default-badges/")
+#def setup_default_badges(db: Session = Depends(get_db)):
+#    default_badges = [
+ #       {"name": "First 100 XP", "description": "You reached 100 XP!", "icon": "🏅"},
+#        {"name": "Chatterbox", "description": "You’ve sent 50 messages!", "icon": "💬"},
+#        {"name": "Level Up!", "description": "You reached your first new level!", "icon": "📈"},
+#    ]
+#
+#    created = 0
+#    for badge_data in default_badges:
+#        exists = db.query(Badge).filter(Badge.name == badge_data["name"]).first()
+#        if not exists:
+#            db.add(Badge(**badge_data))
+#            created += 1
+#
+#    db.commit()
+#    return {"message": f"{created} badges added successfully."}
