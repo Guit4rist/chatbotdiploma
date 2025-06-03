26ecddaa44a0d35869e11136c9bab729be2d4a3a
rauan <salkenov.rauan7@gmail.com>
2025-06-03 22:36:31 +0500

    loading check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,9 +28,7 @@ function App() {
             <Route
               path="/chat"
               element={
-                <ProtectedRoute>
                   <Chat />
-                </ProtectedRoute>
               }
             />
             <Route
